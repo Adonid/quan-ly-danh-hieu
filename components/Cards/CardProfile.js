0b8978c31e5c0f1bdacc5cfb53200077500e4bac
LeHieu <Xavan912@gmail.com>
2021-12-30 21:32:12 +0700

    sua CardProfile

--- components/Cards/CardProfile.js
+++ components/Cards/CardProfile.js
@@ -21,58 +21,59 @@ export default function CardProfile() {
               <div className="flex justify-center py-4 lg:pt-4 pt-8">
                 <div className="mr-4 p-3 text-center">
                   <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                    22
+                    02
                   </span>
-                  <span className="text-sm text-blueGray-400">Friends</span>
+                  <span className="text-sm text-blueGray-400">DH đã nhận</span>
                 </div>
                 <div className="mr-4 p-3 text-center">
                   <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
                     10
                   </span>
-                  <span className="text-sm text-blueGray-400">Photos</span>
-                </div>
-                <div className="lg:mr-4 p-3 text-center">
-                  <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                    89
-                  </span>
-                  <span className="text-sm text-blueGray-400">Comments</span>
+                  <span className="text-sm text-blueGray-400">DH nhận tiếp theo</span>
                 </div>
               </div>
             </div>
           </div>
-          <div className="text-center mt-12">
+          <div className="text-center mt-2">
             <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-              Jenna Stones
+              Nguyễn Huy Hoàng
             </h3>
-            <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+            <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-normal">
+              <i className="fas fa-star mr-2 text-lg text-blueGray-400"></i>{" "}
+                Còn <b> xy ngày</b> nữa sẽ nhận danh hiệu
+                <cite> Tinh anh 1</cite>
+            </div>
+            <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-normal">
               <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>{" "}
-              Los Angeles, California
+              Kp 6, P. An Thới, TP. Phú Quốc, Tỉnh Kiên Giang
             </div>
             <div className="mb-2 text-blueGray-600 mt-10">
               <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
-              Solution Manager - Creative Tim Officer
+              Chức danh - chuyên ngành
             </div>
             <div className="mb-2 text-blueGray-600">
               <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>
-              University of Computer Science
+              Địa chỉ làm việc
+            </div>
+          </div>
+          <div className="mt-10 py-5 border-t border-blueGray-200 text-center">
+            <div className="flex flex-wrap justify-center">
+              <div className="w-full lg:w-9/12 px-4">
+                <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
+                  Tóm tắt tiểu sử.
+                </p>
+              </div>
             </div>
           </div>
           <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
             <div className="flex flex-wrap justify-center">
               <div className="w-full lg:w-9/12 px-4">
-                <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                  An artist of considerable range, Jenna the name taken by
-                  Melbourne-raised, Brooklyn-based Nick Murphy writes, performs
-                  and records all of his own music, giving it a warm, intimate
-                  feel with a solid groove structure. An artist of considerable
-                  range.
-                </p>
                 <a
                   href="#pablo"
-                  className="font-normal text-lightBlue-500"
+                  className="font-normal text-red-500"
                   onClick={(e) => e.preventDefault()}
                 >
-                  Show more
+                  Xóa người dùng này
                 </a>
               </div>
             </div>
