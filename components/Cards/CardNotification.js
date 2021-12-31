46a8a373c098cd28bc828e9f1c1e4127bb21c611
LeHieu <Xavan912@gmail.com>
2021-12-31 09:07:20 +0700

    Xong giao dien 2 bang user & notifiation

--- components/Cards/CardNotification.js
+++ components/Cards/CardNotification.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // components
+
 export default function CardTable({ color }) {
   return (
     <>
@@ -20,8 +21,8 @@ export default function CardTable({ color }) {
                   (color === "light" ? "text-blueGray-700" : "text-white")
                 }
               >
-                <i className="fas fa-users text-blueGray-500 mr-2"></i>
-                Danh sách người dùng
+                <i className="fas fa-bullhorn mr-2"></i>
+                Thông báo nhận danh hiệu
               </h3>
             </div>
           </div>
@@ -49,26 +50,6 @@ export default function CardTable({ color }) {
                       : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
                   }
                 >
-                  Chức danh
-                </th>
-                <th
-                  className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
-                  }
-                >
-                  Nơi làm việc
-                </th>
-                <th
-                  className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
-                  }
-                >
                   Danh hiệu hiện tại
                 </th>
                 <th
@@ -79,7 +60,17 @@ export default function CardTable({ color }) {
                       : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
                   }
                 >
-                  Danh hiệu tiếp theo
+                  Danh hiệu được nhận
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  Đến hạn nhận
                 </th>
                 <th
                   className={
@@ -109,24 +100,28 @@ export default function CardTable({ color }) {
                   </span>
                 </th>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  NV Thiết kế Đồ họa
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  Phòng Kỹ thuật
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                   <i className="fas fa-star text-emerald-500 mr-2"></i> Nhân viên
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                   <i className="fas fa-star text-lightBlue-500 mr-2"></i> Trưởng nhóm
                 </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  Đã quá hạn 3 tuần
+                </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
                   <button 
-                    className="bg-blueGray-300 active:bg-blueGray-200 px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-2 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
                     type="button"
-                    title="Chi tiết"
+                    title="Ẩn thông báo người dùng này"
                   >
-                    <i className="fas fa-user-cog text-blueGray-500 text-sm"></i>
+                    <i className="fas fa-eye text-blueGray-200 text-sm"></i>
+                  </button>
+                  <button 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-2 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    type="button"
+                    title="Trao tặng"
+                  >
+                    <i className="fas fa-angle-double-up text-blueGray-200 text-sm"></i>
                   </button>
                 </td>
               </tr>
@@ -147,24 +142,28 @@ export default function CardTable({ color }) {
                   </span>
                 </th>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  QL Nhân sự ngoài
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  Phòng nhân sự
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                   <i className="fas fa-star text-red-500 mr-2"></i> Quản lý
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                   <i className="fas fa-star text-orange-500 mr-2"></i> Trưởng Phòng
                 </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  Đã quá hạn 2 ngày
+                </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
                   <button 
-                    className="bg-blueGray-300 active:bg-blueGray-200 px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-2 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
                     type="button"
-                    title="Chi tiết"
+                    title="Hiện thông báo cho người dùng này"
                   >
-                    <i className="fas fa-user-cog text-blueGray-500 text-sm"></i>
+                    <i className="fas fa-eye text-blueGray-500 text-sm"></i>
+                  </button>
+                  <button 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-2 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    type="button"
+                    title="Trao tặng"
+                  >
+                    <i className="fas fa-angle-double-up text-blueGray-200 text-sm"></i>
                   </button>
                 </td>
               </tr>
@@ -185,24 +184,28 @@ export default function CardTable({ color }) {
                   </span>
                 </th>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  Trưởng phòng
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  CTy AB
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                   <i className="fas fa-star text-orange-500 mr-2"></i> Trưởng phòng NS
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                   <i className="fas fa-star text-purple-600 mr-2"></i> Giám đốc
                 </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  Hôm nay
+                </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
                   <button 
-                    className="bg-blueGray-300 active:bg-blueGray-200 px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-2 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
                     type="button"
-                    title="Chi tiết"
+                    title="Ẩn thông báo người dùng này"
                   >
-                    <i className="fas fa-user-cog text-blueGray-500 text-sm"></i>
+                    <i className="fas fa-eye text-blueGray-200 text-sm"></i>
+                  </button>
+                  <button 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-2 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    type="button"
+                    title="Trao tặng"
+                  >
+                    <i className="fas fa-angle-double-up text-blueGray-200 text-sm"></i>
                   </button>
                 </td>
               </tr>
--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -3,6 +3,7 @@ import React from "react";
 // components
 
 import CardTable from "components/Cards/CardTable.js";
+import CardNotification from "components/Cards/CardNotification.js";
 
 // layout for page
 
@@ -16,7 +17,7 @@ export default function Tables() {
           <CardTable />
         </div>
         <div className="w-full mb-12 px-4">
-          <CardTable color="dark" />
+          <CardNotification color="dark" />
         </div>
       </div>
     </>
