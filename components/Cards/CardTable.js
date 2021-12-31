ea374ea0dbe318543b908bed01361b2fbb5fb925
LeHieu <Xavan912@gmail.com>
2021-12-31 12:23:20 +0700

    Bat su kien scroll bottom div thanh cong de nap du lieu

--- components/Cards/CardTable.js
+++ components/Cards/CardTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef, useState } from "react";
 import PropTypes from "prop-types";
 import { createPopper } from '@popperjs/core';
 
@@ -21,6 +21,18 @@ export default function CardTable({ color }) {
   const actionFilter = e => {
     e.preventDefault()
     dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover()
+  }
+  // Scroll table
+  const [countLoading, setCountLoading] = useState(0)
+  const myRef = createRef()
+  const indexLoad = 207
+  const onScroll = () => {
+    const scrollT = myRef.current.scrollTop
+    const countToBottom = scrollT / indexLoad
+    if(scrollT % indexLoad === 0 && scrollT !== 0 && countToBottom > countLoading){
+      console.log("Trigger event Loading data", "Number Loading: " + (countToBottom+1))
+      setCountLoading(countToBottom)
+    }
   }
   return (
     <>
@@ -143,7 +155,12 @@ export default function CardTable({ color }) {
             </div>
           </div>
         </div>
-        <div className="block w-full overflow-x-auto">
+        <div 
+          className="block w-full overflow-x-auto" 
+          style={{maxHeight: "555px"}}
+          ref={myRef}
+          onScroll={onScroll}
+        >
           {/* Projects table */}
           <table className="items-center w-full bg-transparent border-collapse">
             <thead>
@@ -209,6 +226,234 @@ export default function CardTable({ color }) {
               </tr>
             </thead>
             <tbody>
+              <tr>
+                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                  <img
+                    src="/img/bootstrap.jpg"
+                    className="h-12 w-12 bg-white rounded-full border"
+                    alt="..."
+                  ></img>{" "}
+                  <span
+                    className={
+                      "ml-3 font-bold " +
+                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                    }
+                  >
+                    Phan Trọng Dũng
+                  </span>
+                </th>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  NV Thiết kế Đồ họa
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  Phòng Kỹ thuật
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-emerald-500 mr-2"></i> Nhân viên
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-lightBlue-500 mr-2"></i> Trưởng nhóm
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                  <button 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    type="button"
+                    title="Chi tiết"
+                  >
+                    <i className="fas fa-user-cog text-blueGray-500 text-sm"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                  <img
+                    src="/img/angular.jpg"
+                    className="h-12 w-12 bg-white rounded-full border"
+                    alt="..."
+                  ></img>{" "}
+                  <span
+                    className={
+                      "ml-3 font-bold " +
+                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                    }
+                  >
+                    Nguyễn Thúy Hằng
+                  </span>
+                </th>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  QL Nhân sự ngoài
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  Phòng nhân sự
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-red-500 mr-2"></i> Quản lý
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-orange-500 mr-2"></i> Trưởng Phòng
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                  <button 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    type="button"
+                    title="Chi tiết"
+                  >
+                    <i className="fas fa-user-cog text-blueGray-500 text-sm"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                  <img
+                    src="/img/sketch.jpg"
+                    className="h-12 w-12 bg-white rounded-full border"
+                    alt="..."
+                  ></img>{" "}
+                  <span
+                    className={
+                      "ml-3 font-bold " +
+                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                    }
+                  >
+                    Minh Đăng Quang
+                  </span>
+                </th>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  Trưởng phòng
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  CTy AB
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-orange-500 mr-2"></i> Trưởng phòng NS
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-purple-600 mr-2"></i> Giám đốc
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                  <button 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    type="button"
+                    title="Chi tiết"
+                  >
+                    <i className="fas fa-user-cog text-blueGray-500 text-sm"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                  <img
+                    src="/img/bootstrap.jpg"
+                    className="h-12 w-12 bg-white rounded-full border"
+                    alt="..."
+                  ></img>{" "}
+                  <span
+                    className={
+                      "ml-3 font-bold " +
+                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                    }
+                  >
+                    Phan Trọng Dũng
+                  </span>
+                </th>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  NV Thiết kế Đồ họa
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  Phòng Kỹ thuật
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-emerald-500 mr-2"></i> Nhân viên
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-lightBlue-500 mr-2"></i> Trưởng nhóm
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                  <button 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    type="button"
+                    title="Chi tiết"
+                  >
+                    <i className="fas fa-user-cog text-blueGray-500 text-sm"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                  <img
+                    src="/img/angular.jpg"
+                    className="h-12 w-12 bg-white rounded-full border"
+                    alt="..."
+                  ></img>{" "}
+                  <span
+                    className={
+                      "ml-3 font-bold " +
+                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                    }
+                  >
+                    Nguyễn Thúy Hằng
+                  </span>
+                </th>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  QL Nhân sự ngoài
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  Phòng nhân sự
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-red-500 mr-2"></i> Quản lý
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-orange-500 mr-2"></i> Trưởng Phòng
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                  <button 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    type="button"
+                    title="Chi tiết"
+                  >
+                    <i className="fas fa-user-cog text-blueGray-500 text-sm"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                  <img
+                    src="/img/sketch.jpg"
+                    className="h-12 w-12 bg-white rounded-full border"
+                    alt="..."
+                  ></img>{" "}
+                  <span
+                    className={
+                      "ml-3 font-bold " +
+                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                    }
+                  >
+                    Minh Đăng Quang
+                  </span>
+                </th>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  Trưởng phòng
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  CTy AB
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-orange-500 mr-2"></i> Trưởng phòng NS
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-star text-purple-600 mr-2"></i> Giám đốc
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                  <button 
+                    className="bg-blueGray-300 active:bg-blueGray-200 px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mb-1 ease-linear transition-all duration-150" 
+                    type="button"
+                    title="Chi tiết"
+                  >
+                    <i className="fas fa-user-cog text-blueGray-500 text-sm"></i>
+                  </button>
+                </td>
+              </tr>
               <tr>
                 <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
                   <img
