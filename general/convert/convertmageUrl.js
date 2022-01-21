da783d5d714cd009cd5c9756aaa812e1d4599e76
LeHieu <Xavan912@gmail.com>
2022-01-21 10:44:25 +0700

    Upload anh

--- general/convert/convertmageUrl.js
+++ general/convert/convertmageUrl.js
@@ -3,7 +3,7 @@ import config from "config/config.json"
 
 export function toImageUrl(uri, avatar=true){
     if(avatar)
-     return uri?(config.host + uri):(config.host + "public/images/users/avatars/original/icon-user-default.png")
+     return uri?(config.host + uri):(config.host + "public/images/users/avatars/icon-user-default.png")
     else
      return uri?(config.host + uri):(config.host + "public/images/users/backgrounds/background-pictures.jpg")
 }
